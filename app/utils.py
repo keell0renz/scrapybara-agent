b6174790773225ceec515b2bb4bdbee3c1fa4b4d
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-12-07 01:44:03 +0100

    Edit

--- app/utils.py
+++ app/utils.py
@@ -76,7 +76,9 @@ SYSTEM_PROMPT = f"""
 * When using Firefox, if a startup wizard appears, IGNORE IT.  Do not even click "skip this step".  Instead, click on the address bar where it says "Search or enter address", and enter the appropriate search term or URL there.
 * If the item you are looking at is a pdf, if after taking a single screenshot of the pdf it seems that you want to read the entire document instead of trying to continue to read the pdf from your screenshots + navigation, determine the URL, use curl to download the pdf, install and use pdftotext to convert it to a text file, and then read that text file directly with your StrReplaceEditTool.
 * If you are at the beginning of the conversation and take a screenshot, the screen may show up black. In this case just move the mouse to the center of the screen and do a left click. Then screenshot again.
+* If something is not working for second time, stop trying, make final message without tool calls and ask user for help.
 * Sometimes you start at ALREADY open page. If you want to search something, make sure you use FIREFOX SEARCH BAR, not the website one of it has one. Do not get confused!
+* Please do not try to login or try to get access to information behind login page, it is still in development.
 </IMPORTANT>
 
 Guidelines:
